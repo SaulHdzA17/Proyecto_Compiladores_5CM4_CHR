715632b0eeea102b598c42eb6aa021cececb3044
GibhramCR <gibhramc@gmail.com>
2023-12-18 22:59:36 -0600

    Revert "Update ASDR.java"
    This reverts commit f36743f6c55b763f609a727ae299001bee3ca94d.

--- proyectofinal/parser/ASDR.java
+++ proyectofinal/parser/ASDR.java
@@ -402,7 +402,7 @@ public class ASDR implements Parser{
     }
 
     //BLOCK -> { DECLARATION }
-    private StmtBlock BLOCK(List<Statement> state) throws Exception{
+    private Statement BLOCK(List<Statement> state) throws Exception{
 
         if(hayErrores) throw new Exception("Error en la funcion BLOCK"); //Vereficamos que no haya errores
 
@@ -697,8 +697,8 @@ public class ASDR implements Parser{
 
         if(hayErrores) throw new Exception("Error en la funcion UNARY"); //Vereficamos que no haya errores
 
-        Token operador;
-        Expression llamada;
+        Token operador = null;
+        Expression llamada = null;
 
         //Primera producción: UNARY -> ! UNARY
         if(( this.preanalisis.tipo == TipoToken.BANG )) {
@@ -799,9 +799,11 @@ public class ASDR implements Parser{
             match(TipoToken.LEFT_PAREN);
             Expression expr = EXPRESSION();
             match(TipoToken.RIGHT_PAREN);
-            return new ExpreGrouping(expr);
+            return expr;
         
         }
+
+        return null;
     }
 
     //*********** OTRAS ********************
@@ -819,7 +821,7 @@ public class ASDR implements Parser{
         List<Token> parame = PARAMETERS_OPC();
         match(TipoToken.RIGHT_PAREN);
         List<Statement> state = new ArrayList<>();
-        StmtBlock block = BLOCK(state);
+        Statement block = BLOCK(state);
 
         return new StmtFunction( name, parame, block);
 
